cc835510a3e0904ef5821720a8d4c46919a6604e
cyqresig <moonsunfall@aliyun.com>
2016-07-18 16:43:54 +0800

    fixed wrong annotation

--- Grid.js
+++ Grid.js
@@ -1,5 +1,5 @@
 /*
- * A smart button for react-native apps
+ * A smart sudoku-grid for react-native apps
  * https://github.com/react-native-component/react-native-smart-sudoku-grid/
  * Released under the MIT license
  * Copyright (c) 2016 react-native-component <moonsunfall@aliyun.com>
