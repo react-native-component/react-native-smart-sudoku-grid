62fac8b1149d4ce1c29b37968400fbb2f3b1832a
cyqresig <moonsunfall@aliyun.com>
2016-07-18 15:51:21 +0800

    finished fisrt version

---
+++ Grid.js
@@ -0,0 +1,65 @@
+/*
+ * A smart button for react-native apps
+ * https://github.com/react-native-component/react-native-smart-sudoku-grid/
+ * Released under the MIT license
+ * Copyright (c) 2016 react-native-component <moonsunfall@aliyun.com>
+ */
+
+import React, {
+  Component,
+  PropTypes,
+} from 'react'
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+} from 'react-native'
+
+const { width: deviceWidth } = Dimensions.get('window');
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap',
+  },
+})
+
+export default class Grid extends Component {
+
+  static PropTypes = {
+      rowWidth: PropTypes.number,
+      columnCount: PropTypes.number.isRequired,
+      dataSource: PropTypes.array.isRequired,
+      renderCell: PropTypes.func.isRequired,
+      containerStyle: View.propTypes.style,
+  }
+
+  // 构造
+    constructor(props) {
+      super(props);
+      // 初始状态
+      this.state = {};
+
+      this.columnWidth = (props.rowWidth || deviceWidth) / props.columnCount
+    }
+
+  render() {
+    return (
+      <View style={[this.props.containerStyle, styles.container, {width: this.props.rowWidth,}]}>
+        {this._renderCells()}
+      </View>
+    )
+  }
+
+  _renderCells() {
+      return this.props.dataSource.map((data, index, dataList) => {
+          return (
+            <View style={{width: this.columnWidth, }}>
+              {this.props.renderCell(data, index, dataList)}
+            </View>
+          )
+      })
+  }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # react-native-smart-sudoku-grid
+
+A sudoku grid layout for react-native apps. Written in JS for cross-platform support.
+It works on iOS and Android.
+
+This component is compatible with React Native 0.25 and newer.
+
+## Preview
+
+![react-native-smart-sudoku-grid-preview][1]
+
+## Installation
+
+```
+npm install react-native-smart-sudoku-grid --save
+```
+
+or
+
+```
+npm install @react-native-component/react-native-smart-sudoku-grid --save
+```
+
+## Usage
+
+Install the button from npm with `npm install @react-native-component/react-native-smart-sudoku-grid --save`.
+Then, require it from your app's JavaScript files with `import Button from '@react-native-component/react-native-smart-sudoku-grid'`.
+
+```js
+
+```
+
+## Props
+
+Prop            | Type   | Optional | Default     | Description
+--------------- | ------ | -------- | ----------- | -----------
+rowWidth        | number | Yes      | deviceWidth | determines the width of a row.
+columnCount     | number | No       |             | determines how many columns a row contains.
+dataSource      | array  | No       |             | determines the datasource of grid
+renderCell      | func   | No       |             | A function that returns the grid cell component.
+containerStyle  | style  | Yes      |             | see [react-native documents][2]
+
+
+[1]: http://cyqresig.github.io/img/react-native-smart-sudoku-grid-preview-v1.0.0.gif
+[2]: https://facebook.github.io/react-native/docs/style.html
